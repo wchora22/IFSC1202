49aff578245515eeb6cd39eb2d02662b4d4e4a0e
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-09-06 15:19:36 +0000

    project files

--- 03.project.py
+++ 03.project.py
@@ -1,1 +1,18 @@
-num1= int(input("Enter first number: "))
+num1= float(input("Enter first number: "))
+
+operator=(input("Enter operator (+,-,*,/): "))
+num2 = float(input("Enter second number: "))
+if operator == "+":
+    result = num1+num2
+    print(result)
+elif operator == "-":
+    result = num1-num2
+    print(result)
+elif operator == "*":
+    result = num1*num2
+    print(result)
+elif operator == "/":
+    result = num1/num2
+    print(result)
+else:
+    print("Invalid operator")
