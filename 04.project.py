541a3d188144044d4f373dd538b19ab83b73142e
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-09-13 17:21:19 +0000

    Project

--- 04.project.py
+++ 04.project.py
@@ -1,9 +1,8 @@
 startrange= int(input("Enter the start of the range: "))
 endrange= int (input("Enter the end of the range: "))
-
-n = 13
-for i in range (2,n//2+1):
-    if n%i== 0: 
-        break
-else:
-    print("Prime", n)
+for n in range (startrange, endrange):
+    for i in range (2,n//2+1):
+        if n%i== 0: 
+            break
+    else:
+        print("Prime", n)
--- Whileexercise.py
+++ Whileexercise.py
@@ -1,3 +1,8 @@
-finnumber =0
-
+firstprevious = 1
+secondprevious = 0
 finnumber= int(input("Enter Fibonacci sequence number: "))
+for i in range(2, finnumber+1):
+    currentvalue = firstprevious+secondprevious
+    secondprevious = firstprevious
+    firstprevious = currentvalue
+print(currentvalue)
