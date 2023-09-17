7fbcfe7dfc09763ce2cea380483c86c6c3860c54
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-09-17 18:32:51 +0000

    exercise

--- WhileLoopExercises.py
+++ WhileLoopExercises.py
@@ -6,8 +6,10 @@ n = int(input("Enter a number (CR to quit): "))
 while n != 0:
     if n != 0:
         n2=n
-    if n>n2:
-        n3 += 1
-        n3 = 1
-        n2 = n
+n2= int (input("Enter a number (CR to quit): "))   
+if n>n2:
+      n3= int (input("Enter a number (CR to quit): "))  
+n3 += 1
+n3 = 1
+n2 = n
 print ("Number of values greater than the previous: ", count)
