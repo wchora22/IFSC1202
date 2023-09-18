471b9d901abf86f567bb0cd1cd5e1004f2ba399f
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-09-18 15:36:52 +0000

    project

--- 05.project.py
+++ 05.project.py
@@ -1,4 +1,16 @@
 startrange = int(input("Enter the start of range: "))
 endofrange = int(input("Enter the end of range: "))
-
-    while i in (r%10):
+for r in range(startrange,endofrange+1):
+    n = r
+    length=0
+    while n > 0:
+        n//=10
+        length=length + 1
+    n=r
+    sum=0
+    while n > 0:
+        digit=n%10
+        sum=sum+digit**length
+        n//=10
+    if sum == r:
+        print(r)
---
+++ Exercises 5.py
@@ -0,0 +1,15 @@
+value = input("Enter a number (CR to quit): ")
+if value != "":
+    maximum = int(value)
+    maximumindex = 1
+    currentindex = 1
+    while value != "":
+        if int(value) > maximum:
+            maximum = int(value)
+            maximumindex= currentindex
+            value = input("Enter a number (CR to quit): ")
+            currentindex += 1
+    print("Maximum: {}".format(maximum))
+    print("Index of maximum:{}".format(maximumindex))
+else:
+    print("Sequnece length is 0")
