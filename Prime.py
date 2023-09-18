ccabf1afc433c189ae6a38a79188f349786bf0ed
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-09-18 14:25:52 +0000

    Exercises

--- Prime.py
+++ Prime.py
@@ -1,7 +1,9 @@
-n = 12
+n = int(input("Enter N:"))
+prime = True
 for i in range (2,n//2+1):
-    if n%i== 0: 
-        print("Composite")
-        break
-else:
+    if n % i== 0:
+        prime = False
+if prime:
     print("Prime")
+else:
+    print ("Composite")
