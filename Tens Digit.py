0b810fcf3fa57971314d782968f4b64a4a7ef435
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-09-19 00:18:08 +0000

    exercises

---
+++ Tens Digit.py
@@ -0,0 +1,4 @@
+num=int(input("Enter a number: "))
+digits=num%100
+tens_digit = digits//10
+print("Tens digit: {}".format(tens_digit))
---
+++ last digit.py
@@ -0,0 +1,3 @@
+num= int(input("Enter a number: "))
+last_dig= num%10
+print ("Last digit: {}".format(last_dig))
