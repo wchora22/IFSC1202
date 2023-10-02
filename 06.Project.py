72664d501be483436fa143508bf93cf12506ce83
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-10-02 15:32:33 +0000

    project

--- 06.Project.py
+++ 06.Project.py
@@ -5,7 +5,6 @@ inputrecord=0
 mergerecord=0
 inputfile = open(inputfilename, 'r')
 outputfile = open(outputfilename, 'w')
-inputrecord+=1 
 line = inputfile.readline()
 while line != '':
     if line == "**Insert Merge File Here**\n":
@@ -13,17 +12,19 @@ while line != '':
         mergefile=open(mergefilename, 'r')
         line= mergefile.readline()
         while line != "":
+            recordcount +=1 
             outputfile.write(line)
             mergerecord+=1
             line = mergefile.readline()
         outputfile.write("\n")
         mergefile.close()
     else:
+        inputrecord+=1
         outputfile.write(line)
-    recordcount += 1
+        recordcount +=1
     line = inputfile.readline()
 inputfile.close()
 outputfile.close()
-print("{} input files".format(inputrecord))
-print("{} merge files".format(mergerecord))
-print("{} output files".format(recordcount))
+print("{} input file records".format(inputrecord))
+print("{} merge file records".format(mergerecord))
+print("{} output file records".format(recordcount))
