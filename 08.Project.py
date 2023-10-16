273757e81682b767c12dfa3f9ce7b3b1f92ecaaa
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-10-16 16:17:11 +0000

    project

--- 08.Project.py
+++ 08.Project.py
@@ -1,1 +1,10 @@
-t= input("Enter search term: ")
+import requests
+response = requests.get('https://www.usconstitution.net/const.txt')
+filestring = response.text
+filestringlist = filestring.split("\n")
+while t != ' ':
+    for i in range (len(filestringlist)):
+        if filestringlist[i].find(t) != -1:
+            print(filestringlist[i])
+    t= input("Enter search term: ")
+for i in 
