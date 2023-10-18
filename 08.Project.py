c11a0658d10ecff18fa5848b9df44008ecb464e8
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-10-18 15:48:22 +0000

    Project

--- 08.Project.py
+++ 08.Project.py
@@ -2,9 +2,20 @@ import requests
 response = requests.get('https://www.usconstitution.net/const.txt')
 filestring = response.text
 filestringlist = filestring.split("\n")
+t= input("Enter search term: ")
 while t != ' ':
     for i in range (len(filestringlist)):
         if filestringlist[i].find(t) != -1:
-            print(filestringlist[i])
+ #           print(filestringlist[i])
+            for j in range(i,0,-1):
+                if filestringlist[j] == "":
+                    top = j
+                    break
+            for k in range(i,len(filestringlist)):
+                if filestringlist[k]== "":
+                    bottom = k
+                    break
+            
+            for l in range(top,bottom):
+                print("Line {}: {}".format(l,filestringlist[l]))
     t= input("Enter search term: ")
-for i in range()
