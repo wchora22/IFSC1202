ad2e4ac47c7da7a5d64ceee59ab0e4621bd55de7
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-11-03 16:35:03 +0000

    exercises and project

---
+++ 10.Project.py
@@ -0,0 +1,28 @@
+class Student():
+    def __init__(self,firstname,lastname,tnumber, scores):
+        self.firstname=firstname
+        self.lastname=lastname
+        self.tnumber= tnumber
+        self.scores= scores
+def RunningAverage(self):
+    list = []
+    x=0
+    for x in range(len(self.scores)):
+        if(self.scores[x]=='\n'):
+            a=1
+        elif(self.scores[x]=='\n'):
+            a=2
+        else:
+            list.append(self.scores[x])
+    avg=sum(list)/len(list)
+    return avg
+def TotalAverage(self):
+    list=[]
+    x=0
+    for x in range(len(self.scores)):
+        if(self.scores[x]==''):
+            list.append(0)
+        elif(self.scores[x]=='\n'):
+            list.append(0)
+        else:
+            list.append(self.score[x])
