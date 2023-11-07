e19948116e909d54dff8b02cb40a7da4f0a1a2ae
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-11-07 23:22:09 +0000

    Projects

--- 10.Project.py
+++ 10.Project.py
@@ -4,6 +4,7 @@ class Student():
         self.lastname=lastname
         self.tnumber= tnumber
         self.scores= scores
+
 def RunningAverage(self):
     list = []
     x=0
@@ -26,3 +27,22 @@ def TotalAverage(self):
             list.append(0)
         else:
             list.append(self.score[x])
+    
+    avg= sum(list)/len(list)
+    print(list)
+    return avg
+
+def Lettergrade(self, Student):
+    avg= Student
+    avg = round(avg)
+    if(avg>=90):
+        grade="A"
+    elif(avg>=80 and avg<90):
+        grade="B"
+    elif(avg>=70 and avg<80):
+        grade="C"
+    elif(avg>=60 and avg<70):
+        grade="D"
+    elif(avg<60):
+        grade="F"
+    return grade
--- 11.Project.py
+++ 11.Project.py
@@ -4,3 +4,20 @@ class Student():
         self.firstname=firstname
         self.lastname=lastname
         self.tnumber=tnumber
+        self.grades= grades
+    
+    def RunningAverage(self):
+    list = []
+    x=0
+    for x in range(len(self.scores)):
+        if(self.scores[x]=='\n'):
+            a=1
+            
+        elif(self.scores[x]=='\n'):
+            a=2
+        else:
+            list.append(self.scores[x])
+
+    avg=sum(list)/len(list)
+
+    return avg
