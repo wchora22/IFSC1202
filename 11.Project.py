c18ec9425dd493c65df68733511e84abb6dd6d1b
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-11-13 15:32:47 +0000

    Projects

--- 11.Project.py
+++ 11.Project.py
@@ -1,36 +1,36 @@
 from math import pi
 class Student():
-    def __init__(self,firstname,lastname,tnumber):
-        self.firstname=firstname
-        self.lastname=lastname
-        self.tnumber=tnumber
-        self.grades= grades
+    def __init__(self,firstname,lastname,tnumber,grades):
+        self.Firstname=firstname
+        self.Lastname=lastname
+        self.Tnumber=tnumber
+        self.Grades= grades
     
-def RunningAverage(self):
-    list = []
-    x=0
-    for x in range(len(self.scores)):
-        if(self.scores[x]=='\n'):
-            a=1
+    def RunningAverage(self):
+        list = []
+        x=0
+        for x in range(len(self.scores)):
+            if(self.scores[x]=='\n'):
+                a=1
             
-        elif(self.scores[x]=='\n'):
-            a=2
-        else:
-            list.append(self.scores[x])
+            elif(self.scores[x]=='\n'):
+                a=2
+            else:
+                list.append(self.scores[x])
 
-    avg=sum(list)/len(list)
+        avg=sum(list)/len(list)
 
-    return avg
-def TotalAverage(self):
-    list=[]
-    x=0
-    for x in range(len(self.scores)):
-        if(self.scores[x]==''):
-            list.append(0)
-        elif(self.scores[x]=='\n'):
-            list.append(0)
-        else:
-            list.append(self.scores[x])
-    avg =sum(list)/len(list)
-    print(list)
-    return avg
+        return avg
+    def TotalAverage(self):
+        list=[]
+        x=0
+        for x in range(len(self.scores)):
+            if(self.scores[x]==''):
+                list.append(0)
+            elif(self.scores[x]=='\n'):
+                list.append(0)
+            else:
+                list.append(self.scores[x])
+        avg =sum(list)/len(list)
+        print(list)
+        return avg
