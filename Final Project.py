7f66b1f170a1133cfca4a3903826cdd033ac911d
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-11-27 17:47:18 +0000

    project

--- Final Project.py
+++ Final Project.py
@@ -3,4 +3,33 @@ class User():
         self.Username=username
         self.Password=password
 class Userlist():
-    def __init__(self,)
+    def __init__(self,filename):
+        self.Userlist=[]
+        self.filename=filename
+    def ReadUserFile(self):
+        userfile=open(self.filename,"r")
+        line=userfile.readline()
+        while line != "":
+            x=line.split(",")
+            userobject=User(x[0].strip(), x[1].strip())
+            self.Userlist.append(userobject)
+            line=userfile.readline()
+    def WriterUserFile(self):
+        userfile=open(self.filename, "w")
+    def DisplayUserlist(self):
+        for i in range(len(self.Userlist)):
+            print(self.Userlist[i].Username,self.Userlist[i].Password)
+    def FindUsername(username):
+
+    def ChangePassword(password):
+
+    def AddUser(username,password):
+
+    def DeleteUser(username):
+
+    def Strength(password):
+
+MyUserlist=Userlist("Final Project Passwords.txt")
+MyUserlist.ReadUserFile()
+MyUserlist.DisplayUserlist()
+MyUserlist.WriterUserFile()
