85aa233599a96de731796631270bca34d62d0664
wchora22 <143210492+wchora22@users.noreply.github.com>
2023-11-29 17:07:06 +0000

    exam

--- Exam Three.py
+++ Exam Three.py
@@ -1,4 +1,5 @@
 import math
+from math import pi
 class Triangle():
     def __init__(self,side1,side2,side3):
         self.Side1=side1
@@ -15,16 +16,16 @@ class Triangle():
         return self.Side1+self.Side2+self.Side3
     def area(self):
         if (self.triangle_type()=="Equilateral"):
-            return (math.sqrt(3)/4)*(self.side1*self.side1)
+            return (math.sqrt(3)/4)*(self.Side1*self.Side1)
         elif (self.triangle_type()=="Isoceles"):
             a=0.00
             b=0.00
-            if (self.side1==self.side2):
-                a=self.side1
-                b-self.side3
+            if (self.Side1==self.Side2):
+                a=self.Side1
+                b-self.Side3
             else:
-                a=self.side1
-                b=self.side2
+                a=self.Side1
+                b=self.Side2
             return (b*math.sqrt((4*a*a)-(b*b)))/4
         else:
             s=self.perimeter()/2
@@ -36,15 +37,43 @@ class Triangle():
             b=0.00
             if (self.Side1==self.Side2):
                 b=self.Side3
-                h= math.sqrt(math.pow(self.Side2))
+                h= math.sqrt(math.pow(self.Side1,2)-math.pow(b/2,2))
+                angle1= (math.acos((b/2*b/2+self.Side1*self.Side1-h*h)/(b*self.Side1)))*180/pi;
+                angle2=angle1
+                angle3=180-angle1-angle2
+                return [angle1,angle2,angle3]
+            elif (self.Side2==self.Side3):
+                b=self.Side1
+                h= math.sqrt(math.pow(self.Side2,2)-math.pow(b/2,2))
+                angle1= (math.acos((b/2*b/2+self.Side2*self.Side2-h*h)/(b*self.Side1)))*180/pi;
+                angle2=angle1
+                angle3=180-angle1-angle2
+                return [angle1,angle2,angle3]
+            else:
+                b=self.Side2
+                h=math.sqrt(math.pow(self.Side1,2)-math.pow(b/2,2))
+                angle1= (math.acos((b/2*b/2+self.Side1*self.Side1-h*h)/(b*self.Side1)))*180/pi;
+                angle2=angle1
+                angle3=180-angle1-angle2
+                return [angle2,angle1,angle3]
+        else:
+            b=self.Side3
+            h=2*(self.area()/b)
+            a1= math.sqrt(math.pow(self.Side1,2)-math.pow(h,2))
+            a2=b-a1
+            angle1=(math.acos((a1*a1+self.Side1*self.Side1-h*h)/(2*a1*self.Side1)))*180/pi;
+            angle2=angle1
+            angle3=180-angle1-angle2
+            return [angle1,angle2,angle3]
+
 obj=[]
 filename=open("Exam Three Triangles.txt", 'r')
 for x in filename:
     values= x.split(',')
     s1= float(values[0])
     s2= float(values[1])
-    s3= float(values [2])
+    s3= float(values[2])
     obj.append(Triangle(s1,s2,s3))
 print('{:>20}{:>12}{:>12}{:>12}{:>12}{:>8}{:>13}{:>12}{:>12}'.format('Type','Side 1','Side 2','Side 3', 'Perimeter', 'Area', 'Angle 1', 'Angle 2', 'Angle 3'))
 for item in obj:
-    print('{:>20}{:>12}{:>12}{:>12}{:>12}{:>8}{:>13}{:>12}{:>12}'.format(item.triangle_type,item.Side1,item.Side2,item.Side3,item.perimeter(),item.area()))
+    print('{:>20}{:>12}{:>12}{:>12}{:>12}{:>8}{:>13}{:>12}{:>12}'.format(item.triangle_type(),item.Side1, item.Side2, item.Side3, item.perimeter(), round(item.area(),3), round(item.angles()[0],3), round(item.angles()[1],3),round(item.angles()[2],3)))
